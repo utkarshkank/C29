19b70701101a2bcb970c6a0c6a7e964a569b7207
utkarshkank <80094801+utkarshkank@users.noreply.github.com>
2021-07-01 21:02:17 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,75 @@
+class launcher{
+	constructor(body,anchor)
+	{
+		var options={
+			
+            bodyA:body,
+            pointB:anchor,
+            stiffness:0.05,
+            length:1
+
+			}
+		this.bodyA = body;
+        this.pointB = anchor;
+        this.launcher  = Constraint.create(options);
+		
+		World.add(world, this.launcher);
+	}
+
+    attach(body){
+
+        this.launcher.bodyA = body;
+        }
+        fly(){
+            this.launcher.bodyA = null;
+        }
+
+
+	display()
+	{
+		if(this.launcher.bodyA){
+            var pointA = this.bodyA.position;
+            var pointB = this.pointB;
+            line(pointA.x,pointA.y,pointB.x,pointB.y);
+        }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
